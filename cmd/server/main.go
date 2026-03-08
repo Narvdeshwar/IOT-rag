a7c8def55befff2d4208dfde34609b9e310b9f0b
Sam Soni <sam.07soni@gmail.com>
2026-03-08 21:37:11 +0530

    feat: Added the /query handler in main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,19 +5,29 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/narvdeshwar/IOT-rag/internal/api"
+	"github.com/narvdeshwar/IOT-rag/internal/cache"
 	"github.com/narvdeshwar/IOT-rag/internal/config"
 	"github.com/narvdeshwar/IOT-rag/internal/db"
+	"github.com/narvdeshwar/IOT-rag/internal/embedder"
+	"github.com/narvdeshwar/IOT-rag/internal/llm"
 	"github.com/narvdeshwar/IOT-rag/internal/logger"
+	"github.com/narvdeshwar/IOT-rag/internal/retriever"
 )
 
 func main() {
 	cfg := config.Load()
 	db.Init(context.Background(), cfg.PostgresURL)
 	defer db.Pool.Close()
-
+	cache.Init()
+	svc := &api.Service{
+		Embedder:  embedder.NewEmbedder(),
+		Retriever: &retriever.Retriever{},
+		LLM:       llm.NewLLM(),
+	}
 	r := gin.Default()
 	r.GET("/health", api.HealthHandler)
 	r.GET("/ready", api.HealthHandler)
+	r.POST("/query", api.QueryHandler(svc))
 	logger.L.Info("Server started", "port", cfg.ServerPort)
 	r.Run(":" + cfg.ServerPort)
 }
